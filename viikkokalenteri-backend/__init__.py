bd9b2977cfae55681ff27469baf1383a27f1182b
donqnr <danconar@gmail.com>
2021-11-08 14:55:33 +0200

    Added endpoints

--- viikkokalenteri-backend/__init__.py
+++ viikkokalenteri-backend/__init__.py
@@ -3,35 +3,35 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/v1/calendar/", methods=['POST'])
-def add_entry():
+def add_entry() -> None:
     return jsonify({
     'test': 'test'
     })
 
 @app.route("/v1/calendar/<int:entry_id>", methods=['DELETE'])
-def delete_entry(entry_id):
+def delete_entry(entry_id) -> None:
     return jsonify({
     'test': entry_id
     })
 
 @app.route("/v1/calendar/<int:entry_id>", methods=['PUT'])
-def edit_entry(entry_id):
+def edit_entry(entry_id) -> None:
     return jsonify({
     'test': entry_id
     })
 
 @app.route("/v1/calendar/<int:entry_id>", methods=['GET'])
-def get_entry(entry_id):
+def get_entry(entry_id) -> None:
     return jsonify({
     'test': entry_id
     })
 
 @app.route("/v1/calendar/working", methods=['POST'])
-def set_working_time():
+def set_working_time() -> None:
     pass
 
 @app.route("/v1/calendar/working", methods=['PUT'])
-def update_working_time():
+def update_working_time() -> None:
     pass
 
 if __name__ == "__main__":

c78ad25e001e4dd69ad19e18487a41430852df7d
donqnr <danconar@gmail.com>
2021-11-17 14:44:47 +0200

    Started implementing the endpoint for adding an entry

--- viikkokalenteri-backend/__init__.py
+++ viikkokalenteri-backend/__init__.py
@@ -1,23 +1,51 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
+
+try:
+    from .interface import database as db
+except ImportError as e:
+    print(str(e))
 
 app = Flask(__name__)
 
+# Adding a calendar marking
 @app.route("/v1/calendar/", methods=['POST'])
 def add_entry() -> None:
+
+    connection = db.ConnectionString()
+    calendar = db.Calendar(connection.connection)
+
+    req = request.get_json()
+
+    location = "The Void"
+    info = "No description"
+
+    try:
+        subject = req['subject']
+        owner = req['owner']
+        date = req['date']
+        time = req['time']
+        if 'location' in req:
+            location = req['location']
+        if 'info' in req:
+            info = req['info']
+
+    except KeyError as e:
+        return e, 400
+
+    # Send the request json to the database.
+
+    return make_response(req), 200
+
+@app.route("/v1/calendar/<string:subject>", methods=['DELETE'])
+def delete_entry(subject: str) -> None:
     return jsonify({
-    'test': 'test'
+    'test': subject
     })
 
-@app.route("/v1/calendar/<int:entry_id>", methods=['DELETE'])
-def delete_entry(entry_id) -> None:
+@app.route("/v1/calendar/<string:subject>", methods=['PUT'])
+def edit_entry(subject: str) -> None:
     return jsonify({
-    'test': entry_id
-    })
-
-@app.route("/v1/calendar/<int:entry_id>", methods=['PUT'])
-def edit_entry(entry_id) -> None:
-    return jsonify({
-    'test': entry_id
+    'test': subject
     })
 
 @app.route("/v1/calendar/<int:entry_id>", methods=['GET'])
